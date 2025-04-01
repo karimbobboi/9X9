f8ae7134c9e471a2a5f0e2363b245e4d11ba098c
karimbobboi <abdulkarimbobboi@gmail.com>
2025-04-01 16:29:53 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import {Form} from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import './utils';
 
 function App() {
+  const [manifest, setManifest] = useState('');
+  const [error, setError] = useState<string>("");
+
+  const isValidUrl = (url: string): boolean => {
+    try {
+      new URL(url);
+      return true;
+    } catch {
+      return false;
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (manifest && isValidUrl(manifest)) {
+      console.log("yes");
+    } else {
+      setError("Invalid IIIF Manifest URL.");
+    }
+  };
+
+  useEffect(() => {
+    if(error.length > 0) console.log(error);
+  }, [error]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex justify-content-center align-items-center vh-100">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>{manifest}</Form.Label>
+          <Form.Control type="text" placeholder="" onChange={(event) => setManifest(event?.target.value)} />
+        </Form.Group>
+      </Form>
     </div>
   );
 }
