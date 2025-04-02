e2153a6055022833a83a8352d6ad93db2c4232e1
karimbobboi <abdulkarimbobboi@gmail.com>
2025-04-02 13:43:37 +0100

    extract image info

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 import './utils';
 
 function App() {
-  const [manifest, setManifest] = useState('');
+  const [manifest, setManifest] = useState<string>('');
   const [error, setError] = useState<string>("");
 
   const isValidUrl = (url: string): boolean => {
@@ -17,12 +17,67 @@ function App() {
     }
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (manifest && isValidUrl(manifest)) {
-      console.log("yes");
+      await fetchIIIF();
     } else {
       setError("Invalid IIIF Manifest URL.");
+    }
+  };
+
+  const fetchIIIF = async () => {
+    const manifest_valid = isValidUrl(manifest);
+    if(!manifest_valid) return;
+
+    try {
+      const response = await fetch(manifest);
+      if (!response.ok) throw new Error("Could not fetch manifest.");
+
+      const data = await response.json();
+      await extractImageInfo(data);
+      setError("");
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
+  const extractTiles = async (manifest: any) => {
+    console.log(manifest);
+
+    const image_info = await extractImageInfo(manifest);
+    if(image_info){
+      console.log(image_info);
+    }
+
+  };
+
+  const extractImageInfo = async (manifest: any) => {
+    let max_width, max_height = 0;
+    let tile_w, tile_h = 0;
+    let base_url = "";
+    let image_url: string = "";
+
+    if(manifest.sequences){
+      console.log("extracting relevant info from manifest...");
+      const canvas = manifest.sequences[0].canvases[0];
+      image_url = canvas.images[0].resource["@id"][0];
+      base_url = canvas.images[0].resource.service["@id"];
+      max_width = canvas.width; 
+      max_height = canvas.height;
+      
+      const tiles_info = canvas.images[0].resource.service.tiles[0];
+      if(tiles_info){
+        tile_w = tiles_info.width;
+        tile_h = tiles_info.height || tile_w;
+      }
+    } 
+    
+    return {
+      base_url,
+      image_url,
+      tile_w, tile_h,
+      max_width, max_height
     }
   };
 
